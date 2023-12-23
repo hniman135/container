d66db28fcb6ac2aceacfb6cee76967a514fee6a1
hniman135 <20521594@gm.uit.edu.vn>
2023-12-23 10:59:52 +0700

    update

--- app.py
+++ app.py
@@ -1,52 +1,123 @@
-from flask import Flask, render_template , request , jsonify, send_file
+from flask import Flask, render_template , request , jsonify, send_file, make_response
 from PIL import Image
 import csv
 import os , io , sys
 import numpy as np 
 import cv2
 import base64
+import json
 from yolo_detection_images import runModel
 from datetime import datetime
-last_detection_result = None
+from pymongo import MongoClient
+last_detection_results = None
 app = Flask(__name__)
+# MongoDB and GridFS setup
+client = MongoClient('mongodb://localhost:27017/')
+db = client['Container']
+collection = db['log']
+
 
 ############################################## THE REAL DEAL ###############################################
 @app.route('/detectObject' , methods=['POST'])
 def mask_image():
-	global last_detection_result
-	# print(request.files , file=sys.stderr)
-	file = request.files['image'].read() ## byte file
-	npimg = np.fromstring(file, np.uint8)
-	img = cv2.imdecode(npimg,cv2.IMREAD_COLOR)
-	######### Do preprocessing here ################
-	# img[img > 150] = 0
-	## any random stuff do here
-	################################################
+    global last_detection_results
+    
+    img_files = request.files.getlist('image')
+    results = []
+    ID_nums = []
+    
+    for index, img_file in enumerate(img_files):
+        img = load_image_from_request(img_file)
+        ##################################### THAY ĐOAN NAY BANG MODEL DETECT ID
+        if index == 0:
+            id = run_ID_detection(img)
+            doc_path = 'D:\Learning\container\detect_result.txt'
+            with open(doc_path, 'r', encoding='utf-8') as file:
+                content = file.read()
+            last_ID_result = get_ID_result(content)
+            ID_nums.append(last_ID_result)
+            
+            for ID_num in ID_nums:
+                ID_data = {
+                    'ID': ID_num['ID'],
+                    'time': ID_num['time']
+                }
+            save_to_csv(ID_data)
+        unique_id = ID_nums[0]['ID'] if ID_nums else 'default_id'
+        unique_time = ID_nums[0]['time'] if ID_nums else datetime.now().isoformat()
+        #####################################
+        img = run_object_detection(img)
+         # Convert both original and processed images to base64
 
-	img = runModel(img)
+        # Get current time
+        current_time = datetime.now().isoformat()
+        img_base64 = save_image_to_base64(img)
 
-	img = Image.fromarray(img.astype("uint8"))
-	rawBytes = io.BytesIO()
-	img.save(rawBytes, "JPEG")
-	rawBytes.seek(0)
-	img_base64 = base64.b64encode(rawBytes.read())
- 
-	doc_path = 'D:\Learning\container\detect_result.txt'
-	with open(doc_path, 'r', encoding='utf-8') as file:
-		content = file.read()
-	parts = content.split()
-	last_detection_result = {
+        doc_path = 'D:\Learning\container\detect_result.txt'
+        with open(doc_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+
+        last_detection_result = get_last_detection_result(content, img_base64)
+        results.append(last_detection_result)
+    
+        # MongoDB document
+        for result in results:
+            document = {
+                "ID": result['ID'],  # Replace with actual ID
+                "Time": result['time'],
+                "PostProcessedImage": result['status']
+            }
+
+            # Insert into MongoDB
+            collection.insert_one(document)
+
+
+    
+    response = {
+        'ID': unique_id,
+        'time': unique_time,
+        'images': [{'status': str(result['status'])} for result in results]
+    }
+
+    return jsonify(response)
+
+def load_image_from_request(file):
+    npimg = np.fromstring(file.read(), np.uint8)
+    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+    return img
+
+def run_object_detection(img):
+    # Run your object detection model
+    img = runModel(img)
+    return img
+
+def run_ID_detection(img):
+    return img
+def save_image_to_base64(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(img.astype("uint8"))
+    rawBytes = io.BytesIO()
+    img.save(rawBytes, "JPEG")
+    rawBytes.seek(0)
+    img_base64 = base64.b64encode(rawBytes.read())
+    return img_base64
+
+def get_last_detection_result(content, img_base64):
+    parts = content.split()
+    return {
         'status': str(img_base64),
         'ID': parts[1],  # You can replace this with the actual ID
         'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     }
-	response_data = {
-        'status': str(img_base64),
-        'ID': last_detection_result['ID'],
-        'time': last_detection_result['time']
+
+def get_ID_result(content):
+    parts = content.split()
+    return {
+        'ID': parts[1],  # You can replace this with the actual ID
+        'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     }
-	save_to_csv(response_data)
-	return jsonify(response_data)
+
+# The rest of your code remains unchanged
 
 def save_to_csv(data):
     csv_path = 'D:\Learning\container\history.csv'
@@ -58,14 +129,14 @@ def save_to_csv(data):
 
         if mode == 'w':
             writer.writeheader()
-
+        print("data:", data)
+        for key, value in data.items():
+            print(f"{key}: {value}")
         writer.writerow({'ID': data['ID'], 'time': data['time']})
+
 ##################################################### THE REAL DEAL HAPPENS ABOVE ######################################
 
-@app.route('/test' , methods=['GET','POST'])
-def test():
-	print("log: got at test" , file=sys.stderr)
-	return jsonify({'status':'succces'})
+
 
 @app.route('/history')
 def history():
--- static/index.js
+++ static/index.js
@@ -1,62 +1,92 @@
 window.onload = () => {
-	$('#sendbutton').click(() => {
-		imagebox = $('#imagebox')
-		input = $('#imageinput')[0]
-		if(input.files && input.files[0])
-		{
-			let formData = new FormData();
-			formData.append('image' , input.files[0]);
-			$.ajax({
-				url: "http://localhost:5000/detectObject", // fix this to your liking
-				type:"POST",
-				data: formData,
-				cache: false,
-				processData:false,
-				contentType:false,
-				error: function(data){
-					console.log("upload error" , data);
-					console.log(data.getAllResponseHeaders());
-				},
-				success: function(data){
-					console.log(data);
-					bytestring = data['status']
-					image = bytestring.split('\'')[1]
-					imagebox.attr('src' , 'data:image/jpeg;base64,'+image)
-					displayIDAndTime(data);
-				}
-			});
-		}
-	});
-	$('#logbutton').click(() => {
+    $('#sendbutton').click(() => {
+
+        const input = $('#imageinput')[0];
+
+        if (input.files && input.files.length > 0) {
+            const formData = new FormData();
+            // Append all selected images to the FormData object
+            for (let i = 0; i < input.files.length; i++) {
+                formData.append('image', input.files[i]);
+            }
+            for (const value of formData.values()) {
+                console.log(value);
+            }
+            
+            $.ajax({
+                url: "http://localhost:5000/detectObject", // Update this to your backend endpoint
+                type: "POST",
+                data: formData,
+                cache: false,
+                processData: false,
+                contentType: false,
+                error: function (data) {
+                    console.log("upload error", data);
+                    console.log(data.getAllResponseHeaders());
+                },
+                success: function(data){
+                    console.log(data);
+                    displayIDAndTime(data);
+                    for (let i = 1; i <= 6; i++) {
+                        $('#image-box-' + i).empty();
+                    }
+                    data['images'].forEach((imageData, index) => {
+                        if (index < 6) {  // Limit to the first 6 images
+                            let bytestring = imageData['status'];
+                            let image = bytestring.split('\'')[1]; // Extract the base64 string
+                            let imageElement = $('<img>', {
+                                src: 'data:image/jpeg;base64,' + image,
+                                class: 'processed-image'
+                            });
+                        
+                            // Append to the corresponding image box
+                            $('#image-box-' + (index + 1)).append(imageElement);
+                            
+                        }
+                        
+                    });
+                }
+                
+            });
+        }
+    });
+
+    $('#logbutton').click(() => {
         // Redirect to the /history route
         window.open("http://127.0.0.1:5000/history", "_blank");
     });
 };
-
 function displayIDAndTime(data) {
 	// Assuming the data from the server includes 'ID' and 'time' fields
 	let ID = data['ID'];
 	let time = data['time'];
-  
+    console.log(ID);
+    console.log(time);
 	// Display ID and time in your HTML or update any specific elements
 	$('#ID').text(ID);
 	$('#time').text(time);
   }
+function readUrl(input) {
+    // Clear existing images in all image boxes
+    $('.image-box').empty();
 
-function readUrl(input){
-	imagebox = $('#imagebox')
-	console.log("evoked readUrl")
-	if(input.files && input.files[0]){
-		let reader = new FileReader();
-		reader.onload = function(e){
-			// console.log(e)
-			
-			imagebox.attr('src',e.target.result); 
-			imagebox.height(500);
-			imagebox.width(800);
-		}
-		reader.readAsDataURL(input.files[0]);
-	}
+    if (input.files && input.files.length > 0) {
+        for (let i = 0; i < input.files.length; i++) {
+            const reader = new FileReader();
 
-	
+            reader.onload = function (e) {
+                // Create an image element for each selected file
+                const imgElement = $('<img>');
+                imgElement.attr('src', e.target.result);
+                imgElement.height(150);
+                imgElement.width(200);
+
+                // Append the image element to the corresponding image box
+                const imageBoxId = '#image-box-' + (i + 1);
+                $(imageBoxId).append(imgElement);
+            };
+
+            reader.readAsDataURL(input.files[i]);
+        }
+    }
 }
